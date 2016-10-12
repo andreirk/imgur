a67b9bb5a37b207d11e8da1392da80a1d4e603df
Andrey Sokolov <sokolik_85@list.ru>
2016-10-12 13:05:55 +0700

    add an api with fetch

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,7 @@
 var rootUrl = 'https://api.imgur.com/3/';
 
 var apiKey = '0f9b67ef75f5b29';
+var apiSecret = '15e469455aee3167fc1ce934da40777e6ae82b7c';
 
 module.exports = {
     get: function (url) {
@@ -8,6 +9,9 @@ module.exports = {
             headers:{
                 "Authorization": "Client-ID " + apiKey
             }
-        }); 
+        })
+        .then(function (response) {
+            return response.json(); 
+        })
     }
 }
